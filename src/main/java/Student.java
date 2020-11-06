7a11d38c28e9907ed5656da0f1be0465ed640341
Alessandro Buy <alessandrobuy@berkeley.edu>
2020-11-06 10:01:59 -0800

    Step 5 Done

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,29 +3,23 @@ public class Student {
     private int id;
     private String major;
     private String sex;
-    private int testScore1;
-    private int testScore2;
 
-    public void Student(int i, String m, String s) {
-        this.id = i;
+
+    public Student(String i, String m, String s) {
+        this.id = Integer.parseInt(i.substring(0, i.length() - 2));
         this.major = m;
         this.sex = s;
     }
 
-    public void setTestScore1(int i) {
-        this.testScore1 = i;
-    }
 
-    public void setTestScore2(int i) {
-        this.testScore2 = i;
-    }
-
-    public int getTestScore1() { return testScore1; }
-    public int getTestScore2() { return testScore2; }
     public int getId() { return id;}
     public String getMajor() {return major;}
     public String getSex() {return sex;}
 
+
+    public String toString() {
+        return (String.valueOf(this.id));
+    }
 
 
 
--- src/main/java/TopBloc.java
+++ src/main/java/TopBloc.java
@@ -1,65 +1,129 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ArrayList;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellType;
+
 
 
 public class TopBloc {
 
+    private static ArrayList<Student> students = new ArrayList<Student>();
+    //private static HashMap scores = new HashMap<Student, Float>();
+    private static HashMap scores = new HashMap<Float, Float>();
+
+    private Iterator<Row> setUpIterator(String path) {
+        try {
+            File file = new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            return sheet.iterator();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
+    private void ProcessStudentData() {
+        Iterator<Row> itr = setUpIterator("/Users/Alessandro/Desktop/Turtle-TopBloc/Student Info.xlsx");
+        itr.next();
+        while (itr.hasNext()) {
+            Row row = itr.next();
+            Iterator<Cell> cell = row.cellIterator();
+            Student s = new Student(cell.next().toString(), cell.next().toString(), cell.next().toString());
+            this.students.add(s);
+        }
+    }
 
+    private void processTests() {
+        Iterator<Row> itr = setUpIterator("/Users/Alessandro/Desktop/Turtle-TopBloc/Test Scores.xlsx");
+        itr.next();
+        while (itr.hasNext()) {
+            Row row = itr.next();
+            Iterator<Cell> cell = row.cellIterator();
+            Float id = Float.valueOf(cell.next().toString());
+            Float v = Float.valueOf(cell.next().toString());
+            scores.put(id, v);
+        }
+    }
 
-    public void ProcessData() {
+    private void processRetakes() {
+        Iterator<Row> itr = setUpIterator("/Users/Alessandro/Desktop/Turtle-TopBloc/Test Retake Scores.xlsx");
+        itr.next();
+        while (itr.hasNext()) {
+            Row row = itr.next();
+            Iterator<Cell> cell = row.cellIterator();
+            Float id = Float.valueOf(cell.next().toString());
+            Float score = Float.valueOf(cell.next().toString());
+            if (Float.compare(score, (Float) scores.get(id)) == 1) {
+                this.scores.replace(id, score);
+            }
+
+        }
+    }
+
+    private float calculateAverage(HashMap h) {
+        float total = 0;
+        for (Object value : h.values()) {
+            total+= (float) value;
+        }
+        return total/(h.values().size());
 
     }
+
+    public ArrayList<Integer> getFCSMajors(ArrayList<Student> students) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (Student s : students) {
+            if (s.getMajor().equals("computer science") && s.getSex().equals("F")) {
+                answer.add(s.getId());
+            }
+        }
+        return answer;
+    }
+
 
 
 
     public static void main(String[] args)
     {
-        try
-        {
-            File file = new File("/Users/Alessandro/Desktop/Turtle-TopBloc/Student Info.xlsx");
-            FileInputStream fis = new FileInputStream(file);
+        TopBloc a = new TopBloc();
+        a.ProcessStudentData();
+        //System.out.println(students);
+        a.processTests();
+        //System.out.println(scores);
+        a.processRetakes();
+        //System.out.println(scores);
+        //System.out.println();
+        System.out.println("The class average, after retakes is: ");
+        System.out.print(a.calculateAverage(scores));
+        System.out.println();
+        System.out.println("The IDs of the students that are female computer science majors are: ");
+        System.out.println(a.getFCSMajors(students));
 
 
-
-            //creating Workbook instance that refers to .xlsx file
-            XSSFWorkbook wb = new XSSFWorkbook(fis);
-            XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object
-
-            Iterator<Row> itr = sheet.iterator();    //iterating over excel file
-            itr.next();
-
-            while (itr.hasNext())
-            {
-                Row row = itr.next();
-                Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column
-                while (cellIterator.hasNext())
-                {
-                    Cell cell = cellIterator.next();
-
-                    if(cell.getCellTypeEnum() == CellType.STRING) {
-                        System.out.print(cell.getStringCellValue() + "\t\t\t");
-                        //break;
-                    } else if (cell.getCellTypeEnum() == CellType.NUMERIC) {
-                        System.out.print(cell.getNumericCellValue() + "\t\t\t");
-                        //break;
-                    }
-
-                }
-                System.out.println("");
-            }
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        try {
+            HttpPost request = new HttpPost("http://yoururl");
+            StringEntity params = new StringEntity("details={\"name\":\"xyz\",\"age\":\"20\"} ");
+            request.addHeader("content-type", "application/x-www-form-urlencoded");
+            request.setEntity(params);
+            HttpResponse response = httpClient.execute(request);
+        } catch (Exception ex) {
+        } finally {
+            // @Deprecated httpClient.getConnectionManager().shutdown();
         }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+
+
+
+
+
+
     }
 
 }
